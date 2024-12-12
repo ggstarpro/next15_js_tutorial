bdb57abc40ffc6acd3c7b9cf4805593a90618089
GentaMorino <genta.morino@gmail.com>
2024-12-13 02:39:45 +0900

    10章の内容

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,6 +5,9 @@
 
 import SideNav from '@/app/ui/dashboard/sidenav';
 
+// ppr
+// export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // ppr
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 };
 
 export default nextConfig;
