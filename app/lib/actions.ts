7a2d3a7339768beb3e3086d9c25c005ad18a1869
GentaMorino <genta.morino@gmail.com>
2024-12-15 04:05:46 +0900

    15章の内容

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -174,3 +174,26 @@ export async function deleteInvoice(id: string) {
 }
 
 // [追加の学習として、Server Actionsでのセキュリティについてもっと読むこともできます。]https://nextjs.org/blog/security-nextjs-server-components-actions
+
+
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
